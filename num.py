1ea9d7c9d2ff23c69352b1c5449a7d032842c5d4
Prerana-kumbhar <kumbharprerana20@gmail.com>
2025-02-14 04:01:14 +0000

    commit the first chnages

--- num.py
+++ num.py
@@ -1,8 +1,10 @@
 # Program to check if a number is even or odd
 
-//kumbhar
+
 # Take input from the user
 num = int(input("Enter a number: "))
+
+num1=int(input("Enter a number: "))
 
 # Check if the number is even or odd
 if num % 2 == 0:
